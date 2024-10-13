b50e554c863d7458158824c309323d9367681387
Shubham Rajendra Wankhede <131159639+shubhs83@users.noreply.github.com>
2024-10-13 11:19:36 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,23 @@
+class Program2{
+    public static void main(String[] args){
+	int n=5;
+	for(int i=1;i<=n;i++)
+	{
+	  for(int j=i;j<=n;j++)
+	  {
+	     System.out.print(j);
+	   }
+	System.out.println();
+	}
+    }
+}
+
+/**********output**************
+
+12345
+2345
+345
+45
+5
+
+********************************/
